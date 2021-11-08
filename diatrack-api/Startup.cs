f27548c0eb3806f1bae8f6efb1d21af29b0bf0de
Peter Kimberley <peter@gradata.com.au>
2021-11-09 01:32:31 +1100

    Use response compression

--- diatrack-api/Startup.cs
+++ diatrack-api/Startup.cs
@@ -33,6 +33,8 @@ namespace Diatrack
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddMicrosoftIdentityWebApi(Configuration.GetSection("AzureAdB2C"));
 
+            services.AddResponseCompression();
+
             services
                 .AddControllers()
                 .AddJsonOptions(opts =>
@@ -57,6 +59,8 @@ namespace Diatrack
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseResponseCompression();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();

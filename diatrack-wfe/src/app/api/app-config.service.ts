4ef3fb980a39ae096d87124776105202aa284435
Peter Kimberley <peter@gradata.com.au>
2021-11-11 17:50:18 +1100

    Decrease update poll interval

--- diatrack-wfe/src/app/api/app-config.service.ts
+++ diatrack-wfe/src/app/api/app-config.service.ts
@@ -7,7 +7,7 @@ import {DateTime, Duration} from "luxon";
     providedIn: 'root'
 })
 export class AppConfigService {
-    readonly appUpdateCheckInterval = Duration.fromObject({minutes: 1});
+    readonly appUpdateCheckInterval = Duration.fromObject({minutes: 10});
     readonly autoRefreshEnabled = true;
     readonly refreshInterval = 10000;
     readonly formDebounceInterval = 500;
